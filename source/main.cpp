f959cdd686af4f0c51fc43f0c2b9bf720338e8cb
damien <damienhall04@gmail.com>
2022-02-02 17:19:21 +0000

    fixed text

--- source/main.cpp
+++ source/main.cpp
@@ -28,6 +28,10 @@ int main(void) {
 		int keyspres = keysDown();
 		if (keyspres & KEY_START)
 			_pause = !_pause;
+		if (keyspres & KEY_X)
+			board.AdjustPaddleSpeed(-1);
+		if (keyspres & KEY_Y)
+			board.AdjustPaddleSpeed(1);
 
 		int keys = keysHeld();
 		if (keys & KEY_L)
@@ -38,22 +42,20 @@ int main(void) {
 			board.AdjustBallSpeed(-1);
 		if (keys & KEY_RIGHT)
 			board.AdjustBallSpeed(1);
-		if (keys & KEY_X)
-			board.AdjustPaddleSpeed(-1);
-		if (keys & KEY_Y)
-			board.AdjustPaddleSpeed(1);
 		if (data.px > 0)
 			board.PaddleSetX(data.px);
 
 		if (!_pause)
 			if (board.Update())
 				g_vol = 64;
+
 		board.Draw();
+
 		printf("\x1b[2;0Hatari breakout - damien :)");
 		printf("\x1b[3;0HL + R to move left and right");
 		printf("\x1b[4;0Hor use touchscreen to move");
-		printf("\x1b[5;0HLEFT + RIGHT to adjust ball speed");
-		printf("\x1b[6;0HX + Y to adjust paddle speed");
+		printf("\x1b[5;0HLEFT + RIGHT - ball speed");
+		printf("\x1b[6;0HX + Y - paddle speed");
 		printf("\x1b[7;0START to pause");
 
 
